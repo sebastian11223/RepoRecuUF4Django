81748b4bb20178c3dcd6970bb180d11ea23ab013
sebastian11223 <mcasco@jaumebalmes.net>
2023-06-09 18:33:34 +0200

    hecho que salgan los datos con el render en las views y adaptado las urls

--- Jaumebalmes_Martin/prova/views.py
+++ Jaumebalmes_Martin/prova/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader, context
 
 # Create your views here.
+# def index(request):
+#     template = loader.get_template('index.html')
+#     return HttpResponse(template.render())
+
 def index(request):
+    professor = {"name":"Martín", "surname":"Casco", "age":"20"}
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    dades = template.render({'nombre':professor["name"], 'surname':professor["surname"], 'age':professor["age"]})
+    return HttpResponse(dades)

3cf91995874fc88d1a4d7da20f211cc908bd0a2f
sebastian11223 <mcasco@jaumebalmes.net>
2023-06-11 18:16:14 +0200

    hecha configuracion a la bbdd de postgresql con railway finalmente por errores, hechas migraciones y modelo terminado

--- Jaumebalmes_Martin/Jaumebalmes_Martin/settings.py
+++ Jaumebalmes_Martin/Jaumebalmes_Martin/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Jaumebalmes_Martin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TWqiENWgAQx8it6fzmgG',
+        'HOST': 'containers-us-west-209.railway.app',
+        'PORT': '6598',
     }
 }
 
--- Jaumebalmes_Martin/prova/models.py
+++ Jaumebalmes_Martin/prova/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    subject = models.CharField(max_length=100)
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    grade = models.CharField(max_length=100)
+
